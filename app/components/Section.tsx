c4a85fb99be1a19a565c8f08f0f1736b4d5cec9f
winfredo <winfrednukpe2002@gmail.com>
2024-12-08 20:30:23 +0000

    Creating the sidebar.

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 interface SectionProps {
   name: string;
   description?: string;
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Home from "./components/Home"
 const page = () => {
   return (
     <div>
-      {/* <Header /> */}
       <Home /> 
     </div>
   )
